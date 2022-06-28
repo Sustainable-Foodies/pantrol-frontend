f81809aac5f610401a5fc98963498c14dd56b0b4
Max Gomes <maxgomes92@live.com>
2022-06-28 22:53:22 +0200

    Add floating add button

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
-import ToolbarHeader from './ToolbarHeader';
-import SideMenu from './SideMenu'
+import React, { useState } from "react";
+import ToolbarHeader from "./ToolbarHeader";
+import SideMenu from "./SideMenu";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
-export default function App () {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+const fabStyle = {
+  position: 'absolute',
+  bottom: 16,
+  right: 16,
+}
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div>
       <ToolbarHeader title="Pantrol" onMenuClick={() => setIsMenuOpen(true)} />
       <SideMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+
+      <Fab color="primary" aria-label="add" sx={fabStyle}>
+        <AddIcon />
+      </Fab>
     </div>
-  )
-} 
+  );
+}
