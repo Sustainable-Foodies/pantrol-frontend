f5db29b65179f20dd6e2680c6bc79c6d9fd241bc
Max Gomes <maxgomes92@live.com>
2022-07-03 14:29:58 +0200

    Improve alert

--- src/app/Home/AddItemAlert.js
+++ src/app/Home/AddItemAlert.js
@@ -1,30 +1,38 @@
 import { Button } from "@mui/material";
 import React from "react";
 import { Alert, SpacerVertical } from "../components";
-import {
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function AddItemAlert({ onClose }) {
-  const navigate = useNavigate()
-  const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const openScanPage = () => {
-    navigate('/app/scan' + location.search)
-  }
+    navigate("/app/scan" + location.search);
+  };
 
   const openItemDetailsPage = () => {
-    navigate('/app/item' + location.search)
-  }
+    navigate("/app/item" + location.search);
+  };
 
   return (
     <Alert onClose={onClose}>
       <p>How would you like to add your items?</p>
-      <SpacerVertical height={20} />
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <Button variant="contained" onClick={openScanPage}>Scanning</Button>
-        <Button variant="contained" onClick={openItemDetailsPage}>Manually</Button>
+      <SpacerVertical height={10} />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Button variant="contained" onClick={openScanPage}>
+          Scan my receipt
+        </Button>
+        <SpacerVertical height={20} />
+        <Button variant="contained" onClick={openItemDetailsPage}>
+          Scan a product
+        </Button>
       </div>
     </Alert>
   );
